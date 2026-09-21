501aa3198813dfcf122f82ec5d19cd3035a09663
Halfian <halfihalfian182@gmail.com>
2026-09-21 09:19:15 +0000

    feat: complete Build A Chat App Lab

--- build-a-chat-app/server.js
+++ build-a-chat-app/server.js
@@ -3,3 +3,65 @@ import fs from 'fs';
 import { WebSocketServer } from 'ws';
 
 const PORT = 3001;
+
+const server = http.createServer((req, res) => {
+    fs.readFile('./public/index.html', (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal Server Error");
+            return;
+        };
+
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    })
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (socket, req) => {
+    const username = new URL(req.url, "http://localhost").searchParams.get("username");
+    const systemMessage = {
+        "type": "system", "text": `${username} joined`
+    };
+    const serializedMessage = JSON.stringify(systemMessage);
+
+    wss.clients.forEach((client) => {
+        if (client.readyState === 1) {
+            client.send(serializedMessage)
+        }
+    });
+
+    socket.on('message', (messageData) => {
+        const parsedData = JSON.parse(messageData);
+        const { username, text } = parsedData;
+        const chatMessage = JSON.stringify({
+            type: "chat",
+            username: username,
+            text: text,
+        });
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === 1) {
+                client.send(chatMessage)
+            }
+        });
+    });
+
+    socket.on('close', () => {
+        const leaveMessage = JSON.stringify({
+            type: "system",
+            text: `${username} left` 
+        });
+
+        wss.clients.forEach((client) => {
+            if (client.readyState === 1) {
+                client.send(leaveMessage);
+            }
+        });
+    })
+});
+
+server.listen(PORT, () => {
+    console.log("Chat server running at http://localhost:3001")
+})
